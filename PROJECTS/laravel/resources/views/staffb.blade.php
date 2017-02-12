a6521937c412e44022bae26f56ff92588d296e9b
hidran <hidran@gmail.com>
2017-02-12 19:20:26 +0100

    first commit

--- PROJECTS/laravel/resources/views/staffb.blade.php
+++ PROJECTS/laravel/resources/views/staffb.blade.php
@@ -8,12 +8,30 @@
 
 
 @if($staff)
+   
    <ul>
     @foreach ($staff as $person)
-
-        <li> {{$person['name']}}   {{$person['name']}} </li>
+          
+        <li style="{{$loop->first ?'color:red':''}}  "> {{$loop->remaining}}  {{$loop->last}} {{$person['name']}}   {{$person['name']}} </li>
     
     @endforeach
    </ul>
+    @else 
+    <p>No staff</p>
 @endif
+<ul>
+    @forelse($staff as $person)
+        <li>  {{$person['name']}}   {{$person['name']}} </li>
+    @empty
+        <li>No staff</li>
+    @endforelse
+</ul>
+<h2>Staff for</h2>
+    @for($i=0; $i<count($staff); $i++)
+        {{  $staff[$i]['name']}}<br>
+    @endfor
+<h2>Staff while</h2>
+@while($person = array_shift($staff))
+    {{  $person['name']}}<br>
+@endwhile
     @endsection
