baa6b0cc790b723f3760eae69e436572653dacbc
HIDRAN ARIAS <hidran.arias@eng.it>
2018-08-21 23:31:54 +0200

    pass user to TestMail mailable class

--- PROJECTS/laravel/app/Mail/TestEmail.php
+++ PROJECTS/laravel/app/Mail/TestEmail.php
@@ -6,19 +6,21 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use LaraCourse\Models\User;
 
 class TestEmail extends Mailable
 {
     use Queueable, SerializesModels;
-
+public $user;
     /**
+     * @var $user
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
